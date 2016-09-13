a48e4ea972cee631287531f8275b1da0cdea4e82
Zaturinsky Stas <idc.zaturinskystanislav@gmail.com>
2016-09-13 19:21:46 +0300

    test

--- Strings/examples.php
+++ Strings/examples.php
@@ -10,6 +10,7 @@
 //        $vowels++;
 //    }
 //}
+
 //-------------------------------------------------------------------- Подсчет серий символов
 //function lookandsay($s) {
 //    // Возвращаемое значение инициализируется пустой строкой
@@ -40,7 +41,8 @@
 //    $s = lookandsay($s);
 //    print "$s<br>";
 //}
-//------------------------------ RESULT 
+
+//------------------------------ RESULT
 //1
 //11
 //21
@@ -51,6 +53,7 @@
 //1113213211
 //31131211131221
 //13211311123113112211
+
 //-------------------------------------------------------------------- Обратная перестановка строки по словам
 //$s = "Once upon a time there was a turtle.";
 //print $s . "<br>";
@@ -64,3 +67,87 @@
 ////------------------------------ 2
 //$s = implode(' ',array_reverse(explode(' ',$s)));
 //print $s;
+
+//-------------------------------------------------------------------- Генерирование записей данных с полями фиксированной длины(PACK)
+//Требуется отформатировать записи данных так, чтобы каждое поле занимало//
+//указанное количество символов.
+
+//$books = array( array('Elmer Gantry', 'Sinclair Lewis', 1927),
+//    array('The Scarlatti Inheritance','Robert Ludlum', 1971),
+//    array('The Parsifal Mosaic','William Styron', 1979) );
+//
+//foreach ($books as $book) {
+//    print pack('A100A15A4', $book[0], $book[1], $book[2]) . "<br>";
+//}
+
+//Форматная строка A25A14A4 приказывает pack() преобразовать последующие
+//аргументы в строку из трех фрагментов, дополняемых пробелами: из 25 символов,
+//14 символов и 4 символов. Для полей, дополняемых пробелами в записях фиксированной
+//длины, функция pack() предоставляет компактное решение.
+//------------------------------ RESULT
+//Elmer Gantry Sinclair Lewis 1927
+//The Scarlatti Inheritance Robert Ludlum 1971
+//The Parsifal Mosaic William Styron 1979
+
+//-------------------------------------------------------------------- Генерирование полей фиксированной длины без использования pack()
+//$books = array( array('Elmer Gantry', 'Sinclair Lewis', 1927),
+//    array('The Scarlatti Inheritance','Robert Ludlum', 1971),
+//    array('The Parsifal Mosaic','William Styron', 1979) );
+//
+//foreach ($books as $book) {
+//    $title = str_pad(substr($book[0], 0, 25), 25, '.');
+//    $author = str_pad(substr($book[1], 0, 15), 15, '.');
+//    $year = str_pad(substr($book[2], 0, 4), 4, '.');
+//
+//    print "$title$author$year<br>";
+//}
+
+//-------------------------------------------------------------------- Разбор записей с полями фиксированной длины
+//$booklist = <<<END
+//Elmer Gantry Sinclair Lewis 1927
+//The Scarlatti InheritanceRobert Ludlum 1971
+//The Parsifal Mosaic Robert Ludlum 1982
+//Sophie's Choice William Styron 1979
+//END;
+//
+//$book_array = [];
+//$books = explode("\n",$booklist);
+//
+//for($i = 0, $j = count($books); $i < $j; $i++) {
+//    $book_array[$i]['title'] = substr($books[$i],0,25);
+//    $book_array[$i]['author'] = substr($books[$i],25,15);
+//    $book_array[$i]['publication_year'] = substr($books[$i],40,4);
+//}
+
+//-------------------------------------------------------------------- Разбор записей с полями фиксированной длины fixed_width_substr( )
+//$booklist = <<< END
+//Elmer Gantry Sinclair Lewis 1927
+//The Scarlatti InheritanceRobert Ludlum 1971
+//The Parsifal Mosaic Robert Ludlum 1982
+//Sophie's Choice William Styron 1979
+//END;
+//
+//function fixed_width_substr($fields,$data) {
+//
+//    $r = array();
+//
+//    for ($i = 0, $j = count($data); $i < $j; $i++) {
+//        $line_pos = 0;
+//
+//        foreach($fields as $field_name => $field_length) {
+//            $r[$i][$field_name] = rtrim(substr($data[$i],$line_pos,$field_length));
+//            $line_pos = $field_length;
+//        }
+//    }
+//
+//    return $r;
+//}
+//
+//$books = explode("\n",$booklist);
+//$book_fields = array(
+//    'title' => 25,
+//    'author' => 15,
+//    'publication_year' => 4);
+//
+//$book_array = fixed_width_substr($book_fields,$books);
+
